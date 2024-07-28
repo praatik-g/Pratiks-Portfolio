baa73f9b0a97370d36df0a81a97bfa9a007764e1
Pratik Gajbhiye <101916370+praatik-g@users.noreply.github.com>
2024-07-28 19:07:40 +0530

    Portfolio uploaded

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["STUDENT", "FRONTEND DEVELOPEMENT TRAINEE" ,"FRESHER ", "INTERN"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+});
